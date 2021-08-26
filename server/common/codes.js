80bdda4ad7919f6b53cb87fe7fdfec1ddec77648
nani0979 <52694220+nani0979@users.noreply.github.com>
2021-08-26 14:49:08 +0800

    to update error_codes.

--- server/common/codes.js
+++ server/common/codes.js
@@ -1,14 +1,51 @@
 export default {
-    getFileSuccess :(filename, file)=>({
+    getMetaDataSuccess: (data) => ({
+        error_code: '100',
+        error_message: 'Get meta data success',
+        data
+    }),
+
+    getMetaDataByAreaSuccess : (country, data)=>({
         error_code : '100',
+        error_message : 'Get meta data success',
+        country,
+        data
+    }),
+
+    getFileSuccess: (filename, file) => ({
+        error_code: '100',
         error_message: 'Get file success.',
-        file_name : filename,
+        file_name: filename,
         file,
     }),
 
-    getFileFailed : (filename)=>({
-        error_code : '101',
-        error_message : 'cannot find the file in server, please check name.',
-        file_name : filename,
+    getPictureSuccess: (filename, file) => ({
+        error_code: '100',
+        error_message: ' Get picture success',
+        file_name: filename,
+        file  //TODO I'not sure if it will  work or not.
+    }),
+
+    getMetaDataFailed:()=>({
+        error_code: '101',
+        error_message: 'meta data parse error.',
+    }),
+
+    getMetaDataByCountryFailed:(country)=>({
+        error_code: '101',
+        error_message: 'cannot find the meta of the country, please check name.',
+        country
+    }),
+
+    getFileFailed: (filename) => ({
+        error_code: '102',
+        error_message: 'cannot find the file in server, please check name.',
+        file_name: filename,
+    }),   
+
+    getPictureFailed: (filename) => ({
+        error_code: '103',
+        error_message: 'cannot find the picture in server, please check name',
+        file_name: filename,
     })
 }
